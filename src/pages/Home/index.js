938a126e0d0b6adb22fec6bb733b9b933642d989
ridhoabdurrahman <ridhoabd007@gmail.com>
2022-07-24 14:00:34 +0700

    Add Tab View

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,11 @@
-import {StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
 import React from 'react';
 import {
   ImageBread,
@@ -9,10 +16,58 @@ import {
   ProfileDummy,
 } from '../../assets';
 import {FoodCard, Gap} from '../../components';
+import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
+
+const renderTabBar = (props) => (
+  <TabBar
+    {...props}
+    indicatorStyle={{
+      backgroundColor: '#020202',
+      height: 3,
+      width: '6%',
+      alignItems: 'center',
+      marginLeft: '10%',
+      borderRadius: 3,
+    }}
+    style={{backgroundColor: '#FFFFFF'}}
+    tabStyle={{width: 'auto'}}
+    renderLabel={({route, focused, color}) => (
+      <Text
+        style={{
+          fontFamily: 'Nunito-SemiBold',
+          fontSize: 14,
+          color: focused ? '#020202' : '#8D92A3',
+          marginLeft: 24,
+        }}>
+        {route.title}
+      </Text>
+    )}
+  />
+);
+
+const FirstRoute = () => <View style={{backgroundColor: '#ff4081', flex: 1}} />;
+
+const SecondRoute = () => (
+  <View style={{backgroundColor: '#673ab7', flex: 1}} />
+);
+
+const initialLayout = {width: Dimensions.get('window').width};
 
 const Home = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {key: '1', title: 'New Taste'},
+    {key: '2', title: 'Popular'},
+    {key: '3', title: 'Recommended'},
+  ]);
+
+  const renderScene = SceneMap({
+    1: FirstRoute,
+    2: SecondRoute,
+    3: FirstRoute,
+  });
   return (
-    <View>
+    <View style={styles.page}>
       <View style={styles.headerContainer}>
         <View>
           <Text style={styles.appName}>AmdaCoffee</Text>
@@ -20,16 +75,27 @@ const Home = () => {
         </View>
         <Image source={ProfileDummy} style={styles.photo} />
       </View>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <View style={styles.foodCardWrapper}>
-          <Gap width={24} />
-          <FoodCard image={ImageCoffee} foodName="Ice Cappucino" />
-          <FoodCard image={ImageRamen} foodName="Mie Ramen" />
-          <FoodCard image={ImageCroffle} foodName="Croffle" />
-          <FoodCard image={ImageBurger} foodName="Burger" />
-          <FoodCard image={ImageBread} foodName="Sandwich" />
-        </View>
-      </ScrollView>
+      <View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={styles.foodCardWrapper}>
+            <Gap width={24} />
+            <FoodCard image={ImageCoffee} foodName="Ice Cappucino" />
+            <FoodCard image={ImageRamen} foodName="Mie Ramen" />
+            <FoodCard image={ImageCroffle} foodName="Croffle" />
+            <FoodCard image={ImageBurger} foodName="Burger" />
+            <FoodCard image={ImageBread} foodName="Sandwich" />
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.tabWrapper}>
+        <TabView
+          renderTabBar={renderTabBar}
+          navigationState={{index, routes}}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={initialLayout}
+        />
+      </View>
     </View>
   );
 };
@@ -37,6 +103,9 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
   headerContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -64,5 +133,8 @@ const styles = StyleSheet.create({
   foodCardWrapper: {
     flexDirection: 'row',
     marginVertical: 24,
+  },
+  tabWrapper: {
+    flex: 1,
   },
 });
